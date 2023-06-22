db6aad929dcacfeb8c07902f4a4598b83eb26826
Fernanda-15 <fernandaqp15@gmail.com>
2023-06-22 14:01:46 -0600

    Quitar UIKIT

--- src/js/sitioPublicidad.js
+++ src/js/sitioPublicidad.js
@@ -1,8 +1,3 @@
-import uk from 'uikit'
-import Icons from 'uikit/dist/js/uikit-icons'
-uk.use(Icons)
-
-import 'uikit/dist/css/uikit.min.css'
 import '../css/SitePublicidad.css'
 
 const CodeSuccess = 0;
